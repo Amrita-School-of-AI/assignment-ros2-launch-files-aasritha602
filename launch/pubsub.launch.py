4c40e0649778f62bd31f936fdc7179e02e37534d
Aasritha <aasritha602@users.noreply.github.com>
2026-02-19 19:51:03 +0530

    Added ROS2 pub-sub launch file and installed launch directory

--- launch/pubsub.launch.py
+++ launch/pubsub.launch.py
@@ -1,16 +1,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='ros2_launch_demo',
-            executable='talker',
-            parameters=[{'message_prefix': 'ROS2'}]
-        ),
-        Node(
-            package='ros2_launch_demo',
-            executable='listener'
-        )
-    ])
 
+def generate_launch_description():
+    talker_node = Node(
+        package='ros2_launch_demo',
+        executable='talker',
+        name='talker',
+        output='screen',
+        parameters=[{
+            'message_prefix': 'ROS2'
+        }]
+    )
+
+    listener_node = Node(
+        package='ros2_launch_demo',
+        executable='listener',
+        name='listener',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        talker_node,
+        listener_node
+    ])
