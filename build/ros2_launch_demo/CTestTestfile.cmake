f9790d9b89ae1fc5858478f49d1d54f27e22dbe8
Aasritha <aasritha602@users.noreply.github.com>
2026-02-19 19:54:20 +0530

    Added ROS2 pub-sub launch file and installed launch directory

--- build/ros2_launch_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sachin/assignment-ros2-launch-files-aasritha602
-# Build directory: /home/sachin/assignment-ros2-launch-files-aasritha602/build/ros2_launch_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
